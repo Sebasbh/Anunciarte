19af7c59b55777ea89e43d597e36710b03e4b0dc
anaencinasd <ana.encinasdiaz>
2023-07-19 20:57:58 +0200

    card created

--- anunciarte-app/src/Pages/Home.jsx
+++ anunciarte-app/src/Pages/Home.jsx
@@ -1,32 +1,29 @@
 import Button from "../Components/atoms/Button";
 import "./home.css";
-import HomeImage from "../../assets/home_img.png"
-
+import HomeImage from "./../assets/home_img.png";
+import CardProduct from "../Components/atoms/CardProduct";
 
 export default function Home() {
   return (
     <div className="home">
-        <div>LOGO</div>
+      <div>LOGO</div>
+      <div className="homeMainContainer">
         <div>
-        <div className="homeMainContainer">
-            <img src={HomeImage} alt="AnunciArte" />
+          <img src={HomeImage} alt="AnunciArte" />
         </div>
-        <div>
-        <div>
-            <h1>the place of ART</h1>
-        </div>
-        <div className="homeButtons">
+        <div className="sloganContainer">
+          <div>
+            <h1>
+              the place of <span className="highlight">ART</span>
+            </h1>
+          </div>
+          <div className="homeButtons">
             <Button style="whiteButton" text="Buy" arrow={true} />
-            <Button style="whiteButton" text="Sell" arrow={true}/>
+            <Button style="whiteButton" text="Sell" arrow={true} />
+          </div>
+          <CardProduct />
         </div>
-
-        </div>
-        </div>
-        
-       
-    
-
+      </div>
     </div>
-   
-  )
+  );
 }
