9614fca0a54abacd70928b02b34b6f3aba7a38ea
Sebastian B.H <sebastianbenavidesheins@gmail.com>
2023-07-19 19:49:00 +0200

    filters created

--- anunciarte-app/Backend/Routes/routes.js
+++ anunciarte-app/Backend/Routes/routes.js
@@ -1,11 +1,12 @@
 
 import express from 'express'
-import { createProduct, deleteProduct, getAllProducts, getProduct, updateProduct } from '../Controllers/ProductControllers.js'
+import { createProduct, deleteProduct, getAllProducts, getProduct, updateProduct, getFilteredProducts } from '../Controllers/ProductControllers.js'
 
 const ProductRouter = express.Router()
 
 ProductRouter.get('/', getAllProducts)
 ProductRouter.get('/:id', getProduct)
+ProductRouter.get('/getFilteredProducts', getFilteredProducts)
 ProductRouter.post('/', createProduct)
 ProductRouter.put('/:id', updateProduct)
 ProductRouter.delete('/:id', deleteProduct)
