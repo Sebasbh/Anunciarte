513645026bcd8b4d7e56a286eac1aaaed92c2587
anaencinasd <ana.encinasdiaz>
2023-07-20 21:13:24 +0200

    merging

---
+++ anunciarte-app/src/Pages/Favourites.jsx
@@ -0,0 +1,23 @@
+import illustration from "../assets/pageIllustration.png"
+import Footer from "../Components/atoms/Footer";
+// import CardProduct from "../Components/atoms/CardProduct";
+import "./favourites.css";
+
+
+export default function Favourites() {
+  return (
+   
+  <div className="Favourites">
+        
+        <div className="illustrationContainer">
+          <img src={illustration} alt="" />
+        </div>
+        <div>
+          <h2>FAVOURITES</h2>
+        </div>
+        <div>
+        <div className="GaleryFavourites"></div>
+        {/* <CardProduct/> */}
+        </div>
+        <Footer />
+  </div>)}
