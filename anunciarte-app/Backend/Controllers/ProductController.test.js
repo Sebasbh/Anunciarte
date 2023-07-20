f4ff8a57b05c93e79839205e2f9815f5c7df1024
Sebastian B.H <sebastianbenavidesheins@gmail.com>
2023-07-20 11:30:07 +0200

    test contollers paass :)

--- anunciarte-app/Backend/Controllers/ProductController.test.js
+++ anunciarte-app/Backend/Controllers/ProductController.test.js
@@ -1,22 +1,30 @@
-import { getAllProducts } from "./ProductControllers.js";
+import { getAllProducts, getProduct, createProduct, updateProduct, deleteProduct, getFilteredProducts } from "../Controllers/ProductControllers.js";
 import Product from "../Models/ProductModel.js";
 
 jest.mock('../Models/ProductModel.js');
 
 describe("Product Controller", () => {
-    test("getAllProducts returns all products", async () => {
-        // Arrange
-        const mockProducts = [
-            { name: "Product 1", price: 100 },
-            { name: "Product 2", price: 200 },
-        ];
-        Product.find = jest.fn().mockResolvedValue(mockProducts);
+    let mockReq, mockRes, mockProduct;
 
-        const mockReq = {};
-        const mockRes = {
+    beforeEach(() => {
+        mockProduct = { _id: "1", name: "Product 1", price: 100 };
+
+        mockReq = {
+            params: { id: mockProduct._id },
+            body: mockProduct,
+            query: {}
+        };
+        
+        mockRes = {
             status: jest.fn().mockReturnThis(),
             json: jest.fn()
         };
+    });
+
+    test("getAllProducts returns all products", async () => {
+        // Arrange
+        const mockProducts = [mockProduct];
+        Product.find = jest.fn().mockResolvedValue(mockProducts);
 
         // Act
         await getAllProducts(mockReq, mockRes);
@@ -26,21 +34,40 @@ describe("Product Controller", () => {
         expect(mockRes.json).toHaveBeenCalledWith(mockProducts);
     });
 
-    test("getAllProducts handles error", async () => {
-        // Arrange
-        const mockError = new Error("An error occurred");
-        Product.find = jest.fn().mockRejectedValue(mockError);
+    // Similar tests for the other controller functions
+    test("getProduct returns the correct product", async () => {
+        Product.findById = jest.fn().mockResolvedValue(mockProduct);
+        await getProduct(mockReq, mockRes);
+        expect(mockRes.status).toHaveBeenCalledWith(200);
+        expect(mockRes.json).toHaveBeenCalledWith(mockProduct);
+    });
 
-        const mockReq = {};
-        const mockRes = {
-            status: jest.fn().mockReturnThis(),
-            json: jest.fn()
-        };
+    test("createProduct creates a product", async () => {
+        Product.create = jest.fn().mockResolvedValue(mockProduct);
+        await createProduct(mockReq, mockRes);
+        expect(mockRes.status).toHaveBeenCalledWith(200);
+        expect(mockRes.json).toHaveBeenCalledWith({ message: "Order created correctly!" });
+    });
 
-        // Act
-        await getAllProducts(mockReq, mockRes);
+    test("updateProduct updates a product", async () => {
+        Product.updateOne = jest.fn().mockResolvedValue({ nModified: 1 });
+        await updateProduct(mockReq, mockRes);
+        expect(mockRes.status).toHaveBeenCalledWith(200);
+        expect(mockRes.json).toHaveBeenCalledWith({ message: "Product updated correctly!" });
+    });
 
-        // Assert
-        expect(mockRes.json).toHaveBeenCalledWith({ message: mockError.message });
+    test("deleteProduct deletes a product", async () => {
+        Product.deleteOne = jest.fn().mockResolvedValue({ deletedCount: 1 });
+        await deleteProduct(mockReq, mockRes);
+        expect(mockRes.status).toHaveBeenCalledWith(200);
+        expect(mockRes.json).toHaveBeenCalledWith({ message: "Product deleted correctly!" });
+    });
+
+    test("getFilteredProducts returns filtered products", async () => {
+        const mockProducts = [mockProduct];
+        Product.find = jest.fn().mockResolvedValue(mockProducts);
+        await getFilteredProducts(mockReq, mockRes);
+        expect(mockRes.status).toHaveBeenCalledWith(200);
+        expect(mockRes.json).toHaveBeenCalledWith(mockProducts);
     });
 });
