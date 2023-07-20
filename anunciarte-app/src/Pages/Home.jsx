0625c437cdf8e67a669ae6892fa9c3be13bd6773
anaencinasd <ana.encinasdiaz>
2023-07-20 21:15:41 +0200

    merging with dev

---
+++ anunciarte-app/src/Pages/Home.jsx
@@ -0,0 +1,34 @@
+import Button from "../Components/atoms/Button";
+import "./home.css";
+import HomeImage from "./../assets/home_img.png";
+import Logo from "./../assets/anunciarteLogo02.png";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <div className="logoContainer">
+        <img src={Logo} alt="anunciArte" />
+      </div>
+      <div className="homeMainContainer">
+        <div>
+          <img src={HomeImage} alt="AnunciArte" />
+        </div>
+        <div className="sloganContainer">
+          <div>
+            <h1>
+              the place of   <span className="highlight">ART</span>
+            </h1>
+          </div>
+          <div className="homeButtons">
+            <a href="/products">
+              <Button style="whiteButton" text="Buy" arrow={true} />
+            </a>
+            <a href="/error">
+              <Button style="whiteButton" text="Sell" arrow={true} />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
