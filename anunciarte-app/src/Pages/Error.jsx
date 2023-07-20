b7bda9f61cea18c11a5fba92fb299c1e8ad46a61
anaencinasd <ana.encinasdiaz>
2023-07-20 13:59:27 +0200

    responsive product detail

--- anunciarte-app/src/Pages/Error.jsx
+++ anunciarte-app/src/Pages/Error.jsx
@@ -4,7 +4,7 @@ import errorImage from "./../assets/alert.png";
 export default function Error() {
   return (
    
-    <div className="Error">
+    <div className="errorContainer">
         <div>Logo</div>
         <hi>Oooops¡, Houston¡, tenemos un problema¡¡</hi>
         <img src={errorImage} alt="Alerta de error" />
--- anunciarte-app/src/Pages/ProductDetail.jsx
+++ anunciarte-app/src/Pages/ProductDetail.jsx
@@ -1,7 +1,10 @@
 import Button from "../Components/atoms/Button";
-import Navbar from "../Components/molecules/MobileNavbar";
 import "./productDetail.css";
 import illustration from "../assets/pageIllustration.png"
+import MobileNavbar from "./../Components/molecules/MobileNavbar"
+import Navbar from "./../Components/atoms/Navbar"
+
+
 
 
 
@@ -9,24 +12,36 @@ export default function ProductDetail() {
   return (
    
     <div className="ProductDetail">
-        <Navbar />
+      <Navbar />
+      <div className="mobileOnly">
+        <MobileNavbar />
+      </div>
+      
+      
+    
+    
+        
         <div className="illustrationContainer">
           <img src={illustration} alt="" />
         </div>
-        <div>
+        <div className="mainContainerProduct">
           <div className="galleryContainer">
 
           </div>
           <div className="infoContainer">
             componente con autor, país y precio
             <div className="ButtonsProductsDetail">
-              <Button />
-              <Button />
+              <Button style="blueButton" text="Contact" arrow={false} />
+              <Button style="yellowButton" text="Buy" arrow={false}/>
             </div>
             <h3>DETAILS</h3>
-            COMPONENTE CON TODA LA INFO DEL CUADRO
+            
+            
+    
             COMPONENTE CON INFO ADICIONAL E INSTRUCCIONES
           </div>
+
+          
         
         </div>
     </div>
