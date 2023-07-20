4f551443c7a322090c0fd03aa61da8892563f745
anaencinasd <ana.encinasdiaz>
2023-07-20 16:55:12 +0200

    connected to json

--- anunciarte-app/src/Pages/ProductDetail.jsx
+++ anunciarte-app/src/Pages/ProductDetail.jsx
@@ -3,13 +3,16 @@ import "./productDetail.css";
 import illustration from "../assets/pageIllustration.png"
 import MobileNavbar from "./../Components/molecules/MobileNavbar"
 import Navbar from "./../Components/atoms/Navbar"
-// import ProductList from "../Components/atoms/ProductList";
+import ProductList from "../Components/atoms/ProductList";
 
+
+ 
 
 
 
 
 export default function ProductDetail() {
+  const productId = 2;
   return (
    
     <div className="ProductDetail">
@@ -27,10 +30,11 @@ export default function ProductDetail() {
         </div>
         <div className="mainContainerProduct">
           <div className="galleryContainer">
+            <ProductList show={{ title: true, price: true, description:true }} id={productId} style={"product-container-white"} />
 
           </div>
           <div className="infoContainer">
-          {/* <ProductList title={true} price={true} /> */}
+       
             <div className="ButtonsProductsDetail">
               <Button style="blueButton" text="Contact" arrow={false} />
               <Button style="yellowButton" text="Buy" arrow={false}/>
